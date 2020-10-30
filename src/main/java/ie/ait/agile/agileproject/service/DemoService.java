a1bd23eec18ae095151a1979b378fdca3b4f0e4f
Berchris Requiao <brneto@gmail.com>
2020-10-30 19:48:09 +0000

    Add new files

---
+++ src/main/java/ie/ait/agile/agileproject/service/DemoService.java
@@ -0,0 +1,30 @@
+package ie.ait.agile.agileproject.service;
+
+import ie.ait.agile.agileproject.domain.Demo;
+import ie.ait.agile.agileproject.repository.DemoRepository;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+
+  private final DemoRepository repository;
+
+  DemoService(DemoRepository repository) { this.repository = repository; }
+
+  public List<Demo> getDemoList() { return repository.findAll(); }
+
+  public Demo findDemo(Long id) {
+    return repository.findById(id).orElseThrow(NoSuchElementException::new);
+  }
+
+  public Demo addDemo(Demo demo) {
+    if (repository.findAllByTitle(demo.getTitle()).isEmpty())
+      return repository.save(demo);
+    else
+      throw new IllegalStateException("This demo title already exists");
+  }
+
+}
